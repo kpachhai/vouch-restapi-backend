da731e7c7402c05229cf5cf0d4f41d5d28bfd206
Waqas Ahmed <wakqasahmed@gmail.com>
2020-05-21 17:01:32 +0400

    Added falcon-cors to handle cors

--- restapi/__init__.py
+++ restapi/__init__.py
@@ -4,7 +4,16 @@ from .brokerService   import BrokerService
 from .getTransactions import GetTransactions
 from .callback        import Callback
 
-api = application = falcon.API()
+from falcon_cors import CORS
+
+cors = CORS(
+    allow_all_origins=True,
+    allow_all_headers=True,
+    allow_all_methods=True,
+)
+
+api = application = falcon.API(middleware=[cors.middleware])
+# api = application = falcon.API()
 brokerService = BrokerService()
 
 api.add_route('/start', StartValidation(brokerService))
