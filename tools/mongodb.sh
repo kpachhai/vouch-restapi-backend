88a49a7ec9bb1f888f44ceab5435a5872aa9c096
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-05-21 19:50:43 -0400

    Added requirements.txt

--- tools/mongodb.sh
+++ tools/mongodb.sh
@@ -3,8 +3,10 @@
 docker container stop vouch-mongo || true && docker container rm -f vouch-mongo || true
 
 # start a mongodb docker container
-docker run -d --name vouch-mongo                     \
-    -e MONGO_INITDB_ROOT_USERNAME=mongoadmin          \
-    -e MONGO_INITDB_ROOT_PASSWORD=vouchmongo         \
+docker run -d --name vouch-mongo                        \
+    -v "$PWD/.mongodb-data:/bitnami/mongodb"            \
+    -e MONGODB_USERNAME=mongoadmin                      \
+    -e MONGODB_PASSWORD=vouchmongo                      \
+    -e MONGODB_DATABASE=vouchdb                         \
     -p 27018:27017                                      \
     mongo
