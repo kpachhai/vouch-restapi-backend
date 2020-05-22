74a7ef7e1cd9a3dafd40f5d5422bb5ec68b0d240
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-05-21 21:39:11 -0400

    Fixed volume path

--- tools/mongodb.sh
+++ tools/mongodb.sh
@@ -1,10 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 
 docker container stop vouch-mongo || true && docker container rm -f vouch-mongo || true
 
 # start a mongodb docker container
 docker run -d --name vouch-mongo                        \
-    -v "$PWD/.mongodb-data:/bitnami/mongodb"            \
+    -v "$PWD/.mongodb-data:/data/db"            \
     -e MONGODB_USERNAME=mongoadmin                      \
     -e MONGODB_PASSWORD=vouchmongo                      \
     -e MONGODB_DATABASE=vouchdb                         \
