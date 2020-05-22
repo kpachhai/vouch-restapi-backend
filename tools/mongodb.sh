5a05c6bd0643a6fc89c1a6620c87645304de1461
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-05-21 22:05:23 -0400

    Reverrtting back to working mode

--- tools/mongodb.sh
+++ tools/mongodb.sh
@@ -4,9 +4,7 @@ docker container stop vouch-mongo || true && docker container rm -f vouch-mongo 
 
 # start a mongodb docker container
 docker run -d --name vouch-mongo                        \
-    -v "$PWD/.mongodb-data:/data/db"            \
-    -e MONGODB_USERNAME=mongoadmin                      \
-    -e MONGODB_PASSWORD=vouchmongo                      \
-    -e MONGODB_DATABASE=vouchdb                         \
+    -e MONGO_INITDB_ROOT_USERNAME=mongoadmin                      \
+    -e MONGO_INITDB_ROOT_PASSWORD=vouchmongo                      \
     -p 27018:27017                                      \
     mongo
